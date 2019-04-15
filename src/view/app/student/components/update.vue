dfc7acc70d551cf70f127a58503be28873247adb
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-04-15 18:20:31 +0800

    Update update.vue

--- src/view/app/student/components/update.vue
+++ src/view/app/student/components/update.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
       <!-- 更新角色  -->
-        <Modal v-model="showModal" title="更新角色" :loading="loading" :mask-closable="false" :width="800" :closable="false">
-            <Form ref="updateRole" :model="formItem" :label-width="90" :rules="ruleValidate">
-                <FormItem label="角色名称：" prop="name">
-                    <Input v-model="formItem.name" placeholder="角色名称"></Input>
+        <Modal v-model="showModal" title="更新学生信息" :loading="loading" :mask-closable="false" :width="800" :closable="false">
+            <Form ref="updateStudent" :model="formData" :label-width="90" :rules="rules" style="width:80%;">
+                <FormItem label="姓名" prop="realName">
+                    <Input v-model.trim="formData.realName"></Input>
                 </FormItem>
-            </Form>
-            <Form  :label-width="70">
-                <FormItem label="权限集：" prop="permissions">
-                    <Tree ref="tree" :data="formItem.permissions" show-checkbox></Tree>
+                <FormItem label="学号" prop="stuNum">
+                    <Input v-model.trim="formData.stuNum"></Input>
+                </FormItem>
+                <FormItem label="专业" prop="dictId">
+                <Select v-model="formData.dictId">
+                    <Option v-for="item in dictList" :value="item.id" :key="item.id">{{ item.dictName }}</Option>
+                </Select>
+                </FormItem> 
+                <FormItem label="年级">
+                    <InputNumber :max="2900" :min="1900" v-model="formData.period"></InputNumber>
+                </FormItem>
+                <FormItem label="班级">
+                    <InputNumber :max="99" :min="1" v-model="formData.whatClass" :formatter="value => `${value}班`"
+                    :parser="value => value.replace('班', '')"></InputNumber>
                 </FormItem>
             </Form>
             <div slot="footer">
@@ -21,99 +31,117 @@
 </template>
 <script>
 import { post } from '@/libs/axios-cfg'
+import dayjs from 'dayjs'
 export default {
     data(){
         return {
-            formItem: {
-                    name:'',
-                    permissions: []
+            formData: {
+                realName: '',
+                stuNum: '',
+                dictId: '',
+                period: dayjs().year(),
+                whatClass:1
             },
             showModal:true,
             loading: false,
-            ruleValidate:{
-                name:[
-                    { required: true, message: '角色名称不能为空'}
-                ]
-            }
+            rules: {
+                realName: [{ required: true, message: "姓名不能为空" }],
+                stuNum: [{ required: true, message: "学号不能为空" }],
+                dictId: [{ required: true, message: "请选择专业" }]
+            },
+            dictList: [],
+            isExist:true
         }
     },
     props:{
         updateObject:{
             type:Object,
             default:null
-        },
-        allResource:{
-            type:Array
         }
     },
     created(){
         if(this.updateObject!=null){
-            this.formItem.name = this.updateObject.name;
+            this.formData.realName = this.updateObject.realName;
+            this.formData.stuNum = this.updateObject.stuNum;
+            this.formData.dictId = this.updateObject.groupVO.profession;
+            this.formData.period = this.updateObject.groupVO.period;
+            this.formData.whatClass = this.updateObject.groupVO.whatClass;
         }
-        this.getAllResource()
+        this.getData();
     },
     methods:{
+        async getData(){
+        this.loading = true;
+        try {
+            let res = await post('system/dictionary/list/{dictCode}',null,{
+                dictCode:'INSTITUTE'
+            })
+            this.dictList = res.data;
+        } catch (error) {
+            this.$throw(error)
+        }
+        this.loading = false;
+        },
       cancel(up=false){
           this.$emit('cancel',up);
       },
       ok(){
-        let useList = this.$refs.tree.getCheckedNodes();
-        this.$refs.updateRole.validate((valid) => {
+        // let groupList = this.$refs.tree.getCheckedNodes();
+        this.$refs.updateStudent.validate((valid) => {
             if (valid) {
-                if(useList.length<1){
-                    this.$Notice.destroy();
-                    this.$Notice.warning({
-                        title: '请至少选择一个权限资源',
-                        duration: 2
-                    });
-                }else{
+                // if(groupList.length<1){
+                //     this.$Notice.destroy();
+                //     this.$Notice.warning({
+                //         title: '请至少选择一个用户分组',
+                //         duration: 2
+                //     });
+                // }else{
                     let postObj = {
-                        name:this.formItem.name,
-                        resources:useList
+                        realName: this.formData.realName,
+                        stuNum: this.formData.stuNum,
+                        dictId: this.formData.dictId,
+                        period: this.formData.period,
+                        whatClass: this.formData.whatClass
                     }
-                    this.update(postObj)
-                }
+                    this.isExistGroup(postObj);
+                    if(this.isExist){
+                        alert("进来了")
+                        this.update(postObj)
+                    }else{
+                        this.cancel(true);
+                    }
+                     
+                // }
             }
         })
       },
       async update(data){
         this.loading = true;
         try {
-            let res = await post('/system/role/update/{id}',data,{
+            let res = await post('/app/student/update/{id}',data,{
                 id:this.updateObject.id
             })
-            this.$Message.success("角色 "+data.name+" 更新成功");
+            this.$Message.success("学生信息 "+data.realName+" 更新成功");
             this.cancel(true);
         } catch (error) {
             this.$throw(error)
         }
         this.loading = false;
       },
-      async getAllResource(){
-        let res = JSON.parse(JSON.stringify(this.allResource));
-        this.dealPostData(res)
-        this.formItem.permissions = res;
-      },
-      permissionSync(data,ro){
-        if(data!=null && data.length>0){
-            data.forEach(r => {
-                if(r.id==ro.id){
-                    ro.checked = true;
-                }
-                if(r.children!=null && r.children.length>0){
-                    permissionSync(data,ro)
-                }
-            })
+      async isExistGroup(data){
+        this.loading = true;      
+        try {
+            let res = await post('/app/group/exist',data)
+            let bool = res.data;
+            if(!bool){
+                this.isExist = false;
+                this.$Message.error("暂未有对应用户分组，请先添加");
+                this.cancel(true);
+            }
+        } catch (error) {
+            this.$throw(error)
         }
-      },
-      dealPostData(data){
-          data.forEach(element => {
-              element.title = element.name;
-              this.permissionSync(this.updateObject.resources,element)
-              if(element.children!=null){
-                  this.dealPostData(element.children)
-              }
-          });
+        this.loading = false;
       }
   }
 }
